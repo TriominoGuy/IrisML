49ba649582547ab6dd47a69e441be60aa399dd08
TriominoGuy <46730323+TriominoGuy@users.noreply.github.com>
2019-09-06 14:22:09 +0100

    Add files via upload

--- IrisML.py
+++ IrisML.py
@@ -1,3 +1,4 @@
+#importing all the necessary modules
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -6,11 +7,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
 import scipy
+#import matplotlib.pyplot as plt
 
-namesarray1=["sepal-length", "sepal-width", "petal-length", "petal-width", "Species"]
-data = pd.read_csv("/Users/administrator/Downloads/Iris.csv", names=namesarray1)
+#imports the csv file and converts it to a pandas dataframe, and changes the column names to the one in the array for ease of use later
+column_names=["sepal-length", "sepal-width", "petal-length", "petal-width", "Species"]
+data = pd.read_csv("/Users/administrator/Downloads/Iris.csv", names=column_names)
 print(data.head)
 print(data.describe())
 print(list(data))
@@ -19,15 +21,17 @@ print(list(data))
 #data.hist()
 #plt.show()
 
+#splits the data into training data and testing data
 train, test = train_test_split(data, test_size = 0.9)
 print(train.shape)
 print(test.shape)
 print(test)
 
-namesarray2 = ["sepal-length", "sepal-width", "petal-length", "petal-width"]
-trainx = train[namesarray2]
-testx = test[namesarray2]
+namesarray = ["sepal-length", "sepal-width", "petal-length", "petal-width"]
+trainx = train[namesarray]
+testx = test[namesarray]
 
+#fitting each model with the training data and then making predictions on the test data 
 LRmodel = LogisticRegression(solver = "liblinear", multi_class = "auto")
 LRmodel.fit(trainx, train.Species)
 LRprediction = LRmodel.predict(testx)
@@ -40,6 +44,7 @@ DTCmodel = DecisionTreeClassifier()
 DTCmodel.fit(trainx, train.Species)
 DTCprediction = DTCmodel.predict(testx)
 
+#prints the result of each species prediction for each model and compares it to the actual species
 print("Logistic Regression: ")
 for i in range(test.shape[0]):
     print("Prediction for test data", i, "is", LRprediction[i],", actual species is", list(test["Species"])[i])
@@ -52,6 +57,7 @@ print("Decision Tree Classifier: ")
 for k in range(test.shape[0]):
     print("Prediction for test data", k, "is", DTCprediction[k],", actual species is", list(test["Species"])[k]) 
 
+#prints the accuracy of each model for comparison
 print("Accuracy for Logistic Regression is", metrics.accuracy_score(LRprediction, test.Species))
 print("Accuracy for K Neighbours Classifier is", metrics.accuracy_score(KNCprediction, test.Species))
 print("Accuracy for Decision Tree Classifier is", metrics.accuracy_score(DTCprediction, test.Species))
